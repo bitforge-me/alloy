ca909dabd53ecc5a9dcc4cf9c6fc1a4ab888a270
Alex <aboyle@zap.me>
2022-02-10 14:55:18 +1300

    Remove flutter_icons as not being used in /lib

--- lib/config.dart
+++ lib/config.dart
@@ -2,7 +2,6 @@ import 'package:universal_platform/universal_platform.dart';
 // uncomment if you need to override the app theme
 //import 'package:flutter/material.dart';
 //import 'package:google_fonts/google_fonts.dart';
-//import 'package:flutter_icons/flutter_icons.dart';
 
 import 'package:zapdart/colors.dart';
 
