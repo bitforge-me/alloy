b8231e3148dc390679d1e018353d64cc35bb2424
Alex <aboyle@zap.me>
2022-09-06 14:40:21 +1200

    Stylize the AlertDialog widget we use to fit our Bitforge theme

--- lib/main.dart
+++ lib/main.dart
@@ -210,7 +210,7 @@ class _MyHomePageState extends State<MyHomePage>
         log.info(
             'old version ${cfg.AppVersion}, currently deployed version is $clientVersionDeployed');
         if (UniversalPlatform.isWeb) {
-          askYesNo(context,
+          bitforgeAskYesNo(context,
                   'A newer version has been released would you like to reload?')
               .then((value) {
             if (value) html.window.location.reload();
@@ -276,7 +276,7 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   Future<void> _logout() async {
-    if (await askYesNo(context, 'Are you sure you want to logout?')) {
+    if (await bitforgeAskYesNo(context, 'Are you sure you want to logout?')) {
       await Prefs.nukeAll();
       Phoenix.rebirth(context);
     }
