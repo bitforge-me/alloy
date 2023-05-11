0ce462646e5ce7c7ce51f08555cbbca6ed98b1bf
Daniel Newton <djpnewton@gmail.com>
2023-05-11 20:12:56 +1200

    update to flutter 3.10

--- lib/main.dart
+++ lib/main.dart
@@ -67,8 +67,8 @@ class MyApp extends StatelessWidget {
         brightness: ZapBrightness,
         scaffoldBackgroundColor: ZapBackground,
         appBarTheme: AppBarTheme(centerTitle: true, color: ZapSecondary),
-        // ignore: deprecated_member_use
-        accentColor: ZapPrimary,
+        colorScheme: ColorScheme.fromSwatch(
+            accentColor: ZapPrimary, brightness: ZapBrightness),
         // create text theme with the correct 'brightness' (eg light or dark)
         textTheme:
             ZapTextThemer(ThemeData(brightness: ZapBrightness).textTheme));
